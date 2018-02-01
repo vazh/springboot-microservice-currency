53f2549c8f15f7fe434794944cebee9b956c1d37
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2018-02-01 16:11:27 +0700

    init(*): springboot microservice tutorial part3

---
+++ src/main/java/io/github/vazh/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,46 @@
+package io.github.vazh.microservice.currencyconversionservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CurrencyConversionController {
+    private static Logger log = LoggerFactory.getLogger(CurrencyConversionController.class);
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+                                                  @PathVariable BigDecimal quantity) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversionBean.class,
+                uriVariables
+        );
+
+        CurrencyConversionBean response = responseEntity.getBody();
+
+        return new CurrencyConversionBean(
+                response.getId(),
+                from,
+                to,
+                response.getConversionMultiple(),
+                quantity,
+                quantity.multiply(response.getConversionMultiple()),
+                response.getPort()
+        );
+    }
+}
