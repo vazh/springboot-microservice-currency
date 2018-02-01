744493cadd7571dc60b75bc747ff5991d8ed86e6
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2018-02-01 16:25:12 +0700

    feat(*): add feign client annotation to springboot

--- src/main/java/io/github/vazh/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/io/github/vazh/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.vazh.microservice.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
